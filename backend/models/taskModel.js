eb5635d7ca43b9dcd7f96acaa0509815e453da36
ThrishaliN <thrishaligamage@gmail.com>
2025-06-21 22:31:15 +0530

    Added task model file

---
+++ backend/models/taskModel.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: [true, 'Please add a task title'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['pending', 'in-progress', 'completed'],
+      default: 'pending',
+    },
+    priority: {
+      type: String,
+      enum: ['low', 'medium', 'high'],
+      default: 'medium',
+    },
+    assignedTo: {
+      type: String,
+      default: '', // or required: true if you want to enforce it
+    },
+    deadline: {
+      type: Date,
+      default: null,
+    },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
